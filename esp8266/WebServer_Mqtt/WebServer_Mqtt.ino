196e86476257855a41de2a09cd930c22462d57e2
Miki Jankov <miki.jankov87@gmail.com>
2019-03-05 09:43:12 +0100

    refactor esp ino file. we need to refactor this

--- esp8266/WebServer_Mqtt/WebServer_Mqtt.ino
+++ esp8266/WebServer_Mqtt/WebServer_Mqtt.ino
@@ -8,10 +8,12 @@
 #define ENABLE_MQTT
 #define SETUP_HTTP_SERVER
 #define ONE_BUTTON
+#define ENABLE_EEPROM
 
 #ifdef ONE_BUTTON
 #define RELAY_1_PIN 2
 #define BUTTON_PIN 0
+#define LED 13
 #else
 #define RELAY_1_PIN 2
 #define RELAY_2_PIN 0
@@ -33,18 +35,29 @@
 
   The current state is stored in EEPROM and restored on bootup
 */
-
+#ifdef ONE_BUTTON
 #include <Bounce2.h>
+#endif
+
+#ifdef ENABLE_EEPROM
 #include <EEPROM.h>
+#endif
+
 #include <ESP8266mDNS.h>
+
+#ifdef SETUP_HTTP_SERVER
 #include <ESP8266WebServer.h>
+#endif
+
+#ifdef ENABLE_MQTT
 #include <PubSubClient.h>
+#endif
 
 /*
    Setup SSID and password for wifi access point
 */
-const char* ssid = "SSID";
-const char* password = "PASSWORD";
+const char* ssid = "SSID"; // CHANGE THIS
+const char* password = "PASS"; // CHANGE THIS
 
 /*
    Setup mqtt broker IP address
@@ -53,7 +66,7 @@ const char* password = "PASSWORD";
 #ifdef ENABLE_MQTT
 const char* inTopic = "topic/in";
 const char* outTopic = "topic/out";
-const char* mqtt_server = "192.168.xxx.xxx";
+const char* mqtt_server = "192.168.xxx.xxx"; // CHANGE THIS
 #endif
 
 /*
@@ -82,6 +95,7 @@ void setupPins() {
 #ifdef ONE_BUTTON
   pinMode(BUTTON_PIN, INPUT);     // Initialize the button pin as an input
   pinMode(RELAY_1_PIN, OUTPUT);     // Initialize the relay 1 pin as an output
+  pinMode(LED, OUTPUT);
   debouncer.attach(BUTTON_PIN);   // Use the bounce2 library to debounce the built in button
   debouncer.interval(50);         // Input must be low for 50 ms
 #else
@@ -96,8 +110,8 @@ void publishMsg(const char* topic, char* msg) {
 #endif
 }
 
-void setupHttpServer() {
 #ifdef SETUP_HTTP_SERVER
+void setupHttpServer() {
   webPage += "<h1>ESP8266 Web Server</h1>";
   webPage += "<p>Relay #1 <a href=\"relay1On\"><button>ON</button></a>&nbsp;<a href=\"relay1Off\"><button>OFF</button></a></p>";
 #ifndef ONE_BUTTON
@@ -106,9 +120,7 @@ void setupHttpServer() {
   if (mdns.begin("relay", WiFi.localIP())) {
     Serial.println("MDNS responder started");
   }
-#endif
 
-#ifdef SETUP_HTTP_SERVER
   server.on("/", []() {
     server.send(200, "text/html", webPage);
   });
@@ -116,12 +128,14 @@ void setupHttpServer() {
   server.on("/relay1On", []() {
     server.send(200, "text/html", webPage);
     digitalWrite(RELAY_1_PIN, HIGH);
+    digitalWrite(LED, LOW);
     publishMsg(outTopic, "1");
     delay(1000);
   });
   server.on("/relay1Off", []() {
     server.send(200, "text/html", webPage);
     digitalWrite(RELAY_1_PIN, LOW);
+    digitalWrite(LED, HIGH);
     publishMsg(outTopic, "0");
     delay(1000);
   });
@@ -141,8 +155,8 @@ void setupHttpServer() {
 #endif
   server.begin();
   Serial.println("HTTP server set up");
-#endif
 }
+#endif
 
 #ifdef ENABLE_MQTT
 void callback(char* topic, byte* payload, unsigned int length) {
@@ -159,26 +173,24 @@ void callback(char* topic, byte* payload, unsigned int length) {
     digitalWrite(RELAY_1_PIN, LOW);
     Serial.println("RELAY_1_PIN -> LOW");
     relay1State = LOW;
-    EEPROM.write(0, relay1State);
-    EEPROM.commit();
+    writeToEEPROM(0, relay1State);
   } else if ((char)payload[0] == '1') {
     digitalWrite(RELAY_1_PIN, HIGH);
     Serial.println("RELAY_1_PIN -> HIGH");
     relay1State = HIGH;
-    EEPROM.write(0, relay1State);
-    EEPROM.commit();
+    writeToEEPROM(0, relay1State);
   } else if ((char)payload[0] == '2') {
     relay1State = !relay1State;
     digitalWrite(RELAY_1_PIN, relay1State);
     Serial.print("RELAY_1_PIN -> switched to ");
     Serial.println(relay1State);
-    EEPROM.write(0, relay1State);
-    EEPROM.commit();
+    writeToEEPROM(0, relay1State);
   }
 }
+#endif
 
-void extButton() {
 #ifdef ONE_BUTTON
+void extButton() {
   debouncer.update();
 
   // Call code if Bounce fell (transition from HIGH to LOW) :
@@ -187,7 +199,8 @@ void extButton() {
     // Toggle relay state :
     relay1State = !relay1State;
     digitalWrite(RELAY_1_PIN, relay1State);
-    EEPROM.write(0, relay1State);    // Write state to EEPROM
+    digitalWrite(LED, !relay1State);
+    writeToEEPROM(0, relay1State);    // Write state to EEPROM
 #ifdef ENABLE_MQTT
     if (relay1State == 1) {
       mqttClient.publish(outTopic, "1");
@@ -197,15 +210,18 @@ void extButton() {
     }
 #endif
   }
-#endif
 }
+#endif
 
+#ifdef ENABLE_MQTT
 void reconnect() {
   // Loop until we're reconnected
   while (!mqttClient.connected()) {
     Serial.print("Attempting MQTT connection...");
+    // Create a random client ID
+    String clientId = "ESP8266Client-";
     // Attempt to connect
-    if (mqttClient.connect("ESP8266ClientRelay")) {
+    if (mqttClient.connect(clientId.c_str())) {
       Serial.println("connected");
       // Once connected, publish an announcement...
       mqttClient.publish(outTopic, "ESP8266 booted");
@@ -236,7 +252,9 @@ void setup_wifi() {
 
   while (WiFi.status() != WL_CONNECTED) {
     for (int i = 0; i < 500; i++) {
+#ifdef ONE_BUTTON
       extButton();
+#endif
       delay(1);
     }
     Serial.print(".");
@@ -249,13 +267,16 @@ void setup_wifi() {
 }
 
 void setup() {
+#ifdef ENABLE_EEPROM
   EEPROM.begin(512);              // Begin eeprom to store on/off state
-  setupPins();
   relay1State = EEPROM.read(0);
+#endif
   digitalWrite(RELAY_1_PIN, relay1State);
-
+  setupPins();
   Serial.begin(115200);
+#ifdef SETUP_HTTP_SERVER
   setupHttpServer();
+#endif
   setup_wifi();                   // Connect to wifi
 #ifdef ENABLE_MQTT
   mqttClient.setServer(mqtt_server, 1883);
@@ -268,10 +289,21 @@ void loop() {
   if (!mqttClient.connected()) {
     reconnect();
   }
+  delay(100);
   mqttClient.loop();
 #endif
+#ifdef ONE_BUTTON
   extButton();
+#endif
 #ifdef SETUP_HTTP_SERVER
   server.handleClient();
 #endif
 }
+
+void writeToEEPROM(int addr, bool value) {
+#ifdef ENABLE_EEPROM
+  EEPROM.write(addr, value);
+  EEPROM.commit();
+#endif
+}
+
